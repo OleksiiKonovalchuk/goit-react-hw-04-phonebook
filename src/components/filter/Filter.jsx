bd87dc9c2493ce0f1824ea827968f210b19c2c2b
OleksiiKonovalchuk <108429819+OleksiiKonovalchuk@users.noreply.github.com>
2023-02-05 14:36:27 +0200

    start

---
+++ src/components/filter/Filter.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './filter.module.css';
+const Filter = ({ onFilter }) => {
+  return (
+    <label className={css.label}>
+      <span className={css.span}>Find contacts by name</span>
+      <input
+        className={css.input}
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        onChange={onFilter}
+      />
+    </label>
+  );
+};
+export default Filter;
+Filter.propTypes = { onFilter: PropTypes.func };
